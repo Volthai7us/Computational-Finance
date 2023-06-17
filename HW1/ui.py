7b238ccf0aebb558426f382b5a13217ee1ab8320
Emir Soyturk <emirsytrk@gmail.com>
2023-06-17 12:20:47 +0300

    requirements.txt

--- HW1/ui.py
+++ HW1/ui.py
@@ -46,6 +46,173 @@ class Utils:
 
         return option_price
 
+    @staticmethod
+    def get_portfolio_details():
+        num_options = st.number_input(
+            "Kaç opsiyon kullanacaksınız:",
+            min_value=0, value=1, step=1
+        )
+        num_stocks = st.number_input(
+            "Kaç hisse senedi kullanacaksınız:",
+            min_value=0, value=0, step=1
+        )
+        spot_price = st.number_input(
+            "Spot fiyatı:",
+            value=100.0, step=1.0
+        )
+
+        risk_free_rate = st.number_input(
+            "Risk Free Rate:",
+            value=0.05, step=0.01
+        )
+        volatility = st.number_input(
+            "Volatility:",
+            value=0.25, step=0.01
+        )
+        time = st.number_input(
+            "Time:",
+            value=1.0, step=0.1
+        )
+
+        auto_option_pricing = st.checkbox("Auto Option Pricing", value=True)
+
+        option_details = []
+
+        for i in range(num_options):
+            st.subheader(f"Opsiyon {i+1} Bilgileri")
+            option_type = st.selectbox(
+                f"Opsiyon {i+1} Tipi:",
+                ["CALL", "PUT"]
+            )
+            option_action = st.selectbox(
+                f"Opsiyon {i+1} Alım/Satım:",
+                ["BUY", "SELL"]
+            )
+            strike_price = st.number_input(
+                f"Opsiyon {i+1} Strike fiyatı:",
+                min_value=0.1, value=100.0, step=1.0
+            )
+            option_price = 0
+            if not auto_option_pricing:
+                option_price = st.number_input(
+                    f"Opsiyon {i+1} Opsiyon fiyatı:",
+                    min_value=0.1, value=0.1, step=1.0
+                )
+            option_details.append(
+                (option_type, option_action, strike_price, option_price)
+            )
+
+        return num_options, num_stocks, spot_price, option_details, risk_free_rate, volatility, time, auto_option_pricing
+
+    @staticmethod
+    def black_scholes_analysis():
+        strike_prices = np.arange(70., 150., 1)
+        calls_by_price = [Utils.get_option_price(
+            OptionType.CALL, 100, strike_price) for strike_price in strike_prices]
+        puts_by_price = [Utils.get_option_price(
+            OptionType.PUT, 100, strike_price) for strike_price in strike_prices]
+
+        risk_free_rates = np.arange(0.01, 0.1, 0.01)
+        calls_by_strike = [Utils.get_option_price(
+            OptionType.CALL, 100, 100, risk_free_rate=risk_free_rate) for risk_free_rate in risk_free_rates]
+        puts_by_strike = [Utils.get_option_price(
+            OptionType.PUT, 100, 100, risk_free_rate=risk_free_rate) for risk_free_rate in risk_free_rates]
+
+        times = np.arange(0.1, 2, 0.1)
+        calls_by_time = [Utils.get_option_price(
+            OptionType.CALL, 100, 100, time=time) for time in times]
+        puts_by_time = [Utils.get_option_price(
+            OptionType.PUT, 100, 100, time=time) for time in times]
+
+        volatilities = np.arange(0.1, 1, 0.1)
+        calls_by_volatility = [Utils.get_option_price(
+            OptionType.CALL, 100, 100, volatility=volatility) for volatility in volatilities]
+        puts_by_volatility = [Utils.get_option_price(
+            OptionType.PUT, 100, 100, volatility=volatility) for volatility in volatilities]
+
+        # Create individual plots
+        fig_price = go.Figure()
+        fig_strike = go.Figure()
+        fig_time = go.Figure()
+        fig_volatility = go.Figure()
+
+        fig_price.add_trace(go.Scatter(
+            x=strike_prices, y=calls_by_price, mode='lines', name='Call by Strike Price'))
+        fig_price.add_trace(go.Scatter(
+            x=strike_prices, y=puts_by_price, mode='lines', name='Put by Strike Price'))
+        fig_price.update_layout(title='Option Prices by Strike Price')
+
+        fig_strike.add_trace(go.Scatter(
+            x=risk_free_rates, y=calls_by_strike, mode='lines', name='Call by Risk Free Rate'))
+        fig_strike.add_trace(go.Scatter(
+            x=risk_free_rates, y=puts_by_strike, mode='lines', name='Put by Risk Free Rate'))
+        fig_strike.update_layout(title='Option Prices by Risk Free Rate')
+
+        fig_time.add_trace(go.Scatter(x=times, y=calls_by_time,
+                                      mode='lines', name='Call by Time'))
+        fig_time.add_trace(go.Scatter(x=times, y=puts_by_time,
+                                      mode='lines', name='Put by Time'))
+        fig_time.update_layout(title='Option Prices by Time')
+
+        fig_volatility.add_trace(go.Scatter(
+            x=volatilities, y=calls_by_volatility, mode='lines', name='Call by Volatility'))
+        fig_volatility.add_trace(go.Scatter(
+            x=volatilities, y=puts_by_volatility, mode='lines', name='Put by Volatility'))
+        fig_volatility.update_layout(title='Option Prices by Volatility')
+
+        # Display the plots
+        st.plotly_chart(fig_price, use_container_width=True)
+        st.plotly_chart(fig_strike, use_container_width=True)
+        st.plotly_chart(fig_time, use_container_width=True)
+        st.plotly_chart(fig_volatility, use_container_width=True)
+
+    @staticmethod
+    def get_example_portfolios(risk_free_rate, volatility, time):
+        portfolios = [
+            Portfolio("Bull Bear Spreads", [], [
+                (Option(OptionType.CALL, OrderType.BUY, 80, 100,
+                        False, risk_free_rate, volatility, time), 1),
+                (Option(OptionType.CALL, OrderType.SELL, 120, 100, False, risk_free_rate, volatility, time), 1)]),
+
+            Portfolio("Bear Spreads", [], [
+                (Option(OptionType.CALL, OrderType.SELL, 80, 100,
+                        False, risk_free_rate, volatility, time), 1),
+                (Option(OptionType.CALL, OrderType.BUY, 120, 100, False, risk_free_rate, volatility, time), 1)]),
+
+            Portfolio("Butterfly Spreads", [], [
+                (Option(OptionType.CALL, OrderType.BUY, 80, 100,
+                        False, risk_free_rate, volatility, time), 1),
+                (Option(OptionType.CALL, OrderType.SELL, 100, 100,
+                        False, risk_free_rate, volatility, time), 2),
+                (Option(OptionType.CALL, OrderType.BUY, 120, 100, False, risk_free_rate, volatility, time), 1)]),
+
+            Portfolio("Straddles", [], [
+                (Option(OptionType.CALL, OrderType.BUY, 80, 100,
+                        False, risk_free_rate, volatility, time), 1),
+                (Option(OptionType.PUT, OrderType.BUY, 80, 100, False, risk_free_rate, volatility, time), 1)]),
+
+            Portfolio("Straps", [], [
+                (Option(OptionType.CALL, OrderType.BUY, 80, 100,
+                        False, risk_free_rate, volatility, time), 3),
+                (Option(OptionType.PUT, OrderType.BUY, 80, 100, False, risk_free_rate, volatility, time), 1)]),
+
+            Portfolio("Strips", [], [
+                (Option(OptionType.CALL, OrderType.BUY, 80, 100,
+                        False, risk_free_rate, volatility, time), 1),
+                (Option(OptionType.PUT, OrderType.BUY, 80, 100, False, risk_free_rate, volatility, time), 3)]),
+
+            Portfolio("Strangle", [], [
+                (Option(OptionType.CALL, OrderType.BUY, 80, 100,
+                        False, risk_free_rate, volatility, time), 1),
+                (Option(OptionType.PUT, OrderType.BUY, 120, 100, False, risk_free_rate, volatility, time), 1)]),
+
+            Portfolio("Covered Call", [
+                (Stock(100), 1)],
+                [(Option(OptionType.CALL, OrderType.SELL, 80, 100, False, risk_free_rate, volatility, time), 1)])
+        ]
+
+        return portfolios
+
 
 class Option:
     def __init__(self, option_type, order_type, strike_price, spot_price, option_price=False, risk_free_rate=0.05, volatility=0.25, time=1):
@@ -170,177 +337,6 @@ class Portfolio:
         self.options.append((o, quantity))
 
 
-def get_portfolio_details():
-    num_options = st.number_input(
-        "Kaç opsiyon kullanacaksınız:",
-        min_value=0, value=1, step=1
-    )
-    num_stocks = st.number_input(
-        "Kaç hisse senedi kullanacaksınız:",
-        min_value=0, value=0, step=1
-    )
-    spot_price = st.number_input(
-        "Spot fiyatı:",
-        value=100.0, step=1.0
-    )
-
-    risk_free_rate = st.number_input(
-        "Risk Free Rate:",
-        value=0.05, step=0.01
-    )
-    volatility = st.number_input(
-        "Volatility:",
-        value=0.25, step=0.01
-    )
-    time = st.number_input(
-        "Time:",
-        value=1.0, step=0.1
-    )
-
-    auto_option_pricing = st.checkbox("Auto Option Pricing", value=True)
-
-    option_details = []
-
-    for i in range(num_options):
-        st.subheader(f"Opsiyon {i+1} Bilgileri")
-        option_type = st.selectbox(
-            f"Opsiyon {i+1} Tipi:",
-            ["CALL", "PUT"]
-        )
-        option_action = st.selectbox(
-            f"Opsiyon {i+1} Alım/Satım:",
-            ["BUY", "SELL"]
-        )
-        strike_price = st.number_input(
-            f"Opsiyon {i+1} Strike fiyatı:",
-            min_value=0.1, value=100.0, step=1.0
-        )
-        option_price = 0
-        # if auto_option_pricing:
-        #     option_price = Utils.get_option_price(
-        #         OptionType.CALL if option_type == "CALL" else OptionType.PUT, spot_price, strike_price)
-        #     st.write(f"Opsiyon fiyatı: {option_price}")
-        if not auto_option_pricing:
-            option_price = st.number_input(
-                f"Opsiyon {i+1} Opsiyon fiyatı:",
-                min_value=0.1, value=0.1, step=1.0
-            )
-        option_details.append(
-            (option_type, option_action, strike_price, option_price)
-        )
-
-    return num_options, num_stocks, spot_price, option_details, risk_free_rate, volatility, time, auto_option_pricing
-
-
-def black_scholes_analysis():
-    strike_prices = np.arange(70., 150., 1)
-    calls_by_price = [Utils.get_option_price(
-        OptionType.CALL, 100, strike_price) for strike_price in strike_prices]
-    puts_by_price = [Utils.get_option_price(
-        OptionType.PUT, 100, strike_price) for strike_price in strike_prices]
-
-    risk_free_rates = np.arange(0.01, 0.1, 0.01)
-    calls_by_strike = [Utils.get_option_price(
-        OptionType.CALL, 100, 100, risk_free_rate=risk_free_rate) for risk_free_rate in risk_free_rates]
-    puts_by_strike = [Utils.get_option_price(
-        OptionType.PUT, 100, 100, risk_free_rate=risk_free_rate) for risk_free_rate in risk_free_rates]
-
-    times = np.arange(0.1, 2, 0.1)
-    calls_by_time = [Utils.get_option_price(
-        OptionType.CALL, 100, 100, time=time) for time in times]
-    puts_by_time = [Utils.get_option_price(
-        OptionType.PUT, 100, 100, time=time) for time in times]
-
-    volatilities = np.arange(0.1, 1, 0.1)
-    calls_by_volatility = [Utils.get_option_price(
-        OptionType.CALL, 100, 100, volatility=volatility) for volatility in volatilities]
-    puts_by_volatility = [Utils.get_option_price(
-        OptionType.PUT, 100, 100, volatility=volatility) for volatility in volatilities]
-
-    # Create individual plots
-    fig_price = go.Figure()
-    fig_strike = go.Figure()
-    fig_time = go.Figure()
-    fig_volatility = go.Figure()
-
-    fig_price.add_trace(go.Scatter(
-        x=strike_prices, y=calls_by_price, mode='lines', name='Call by Strike Price'))
-    fig_price.add_trace(go.Scatter(
-        x=strike_prices, y=puts_by_price, mode='lines', name='Put by Strike Price'))
-    fig_price.update_layout(title='Option Prices by Strike Price')
-
-    fig_strike.add_trace(go.Scatter(
-        x=risk_free_rates, y=calls_by_strike, mode='lines', name='Call by Risk Free Rate'))
-    fig_strike.add_trace(go.Scatter(
-        x=risk_free_rates, y=puts_by_strike, mode='lines', name='Put by Risk Free Rate'))
-    fig_strike.update_layout(title='Option Prices by Risk Free Rate')
-
-    fig_time.add_trace(go.Scatter(x=times, y=calls_by_time,
-                       mode='lines', name='Call by Time'))
-    fig_time.add_trace(go.Scatter(x=times, y=puts_by_time,
-                       mode='lines', name='Put by Time'))
-    fig_time.update_layout(title='Option Prices by Time')
-
-    fig_volatility.add_trace(go.Scatter(
-        x=volatilities, y=calls_by_volatility, mode='lines', name='Call by Volatility'))
-    fig_volatility.add_trace(go.Scatter(
-        x=volatilities, y=puts_by_volatility, mode='lines', name='Put by Volatility'))
-    fig_volatility.update_layout(title='Option Prices by Volatility')
-
-    # Display the plots
-    st.plotly_chart(fig_price, use_container_width=True)
-    st.plotly_chart(fig_strike, use_container_width=True)
-    st.plotly_chart(fig_time, use_container_width=True)
-    st.plotly_chart(fig_volatility, use_container_width=True)
-
-
-def get_example_portfolios(risk_free_rate, volatility, time):
-    portfolios = [
-        Portfolio("Bull Bear Spreads", [], [
-            (Option(OptionType.CALL, OrderType.BUY, 80, 100,
-             False, risk_free_rate, volatility, time), 1),
-            (Option(OptionType.CALL, OrderType.SELL, 120, 100, False, risk_free_rate, volatility, time), 1)]),
-
-        Portfolio("Bear Spreads", [], [
-            (Option(OptionType.CALL, OrderType.SELL, 80, 100,
-             False, risk_free_rate, volatility, time), 1),
-            (Option(OptionType.CALL, OrderType.BUY, 120, 100, False, risk_free_rate, volatility, time), 1)]),
-
-        Portfolio("Butterfly Spreads", [], [
-            (Option(OptionType.CALL, OrderType.BUY, 80, 100,
-             False, risk_free_rate, volatility, time), 1),
-            (Option(OptionType.CALL, OrderType.SELL, 100, 100,
-             False, risk_free_rate, volatility, time), 2),
-            (Option(OptionType.CALL, OrderType.BUY, 120, 100, False, risk_free_rate, volatility, time), 1)]),
-
-        Portfolio("Straddles", [], [
-            (Option(OptionType.CALL, OrderType.BUY, 80, 100,
-             False, risk_free_rate, volatility, time), 1),
-            (Option(OptionType.PUT, OrderType.BUY, 80, 100, False, risk_free_rate, volatility, time), 1)]),
-
-        Portfolio("Straps", [], [
-            (Option(OptionType.CALL, OrderType.BUY, 80, 100,
-             False, risk_free_rate, volatility, time), 3),
-            (Option(OptionType.PUT, OrderType.BUY, 80, 100, False, risk_free_rate, volatility, time), 1)]),
-
-        Portfolio("Strips", [], [
-            (Option(OptionType.CALL, OrderType.BUY, 80, 100,
-             False, risk_free_rate, volatility, time), 1),
-            (Option(OptionType.PUT, OrderType.BUY, 80, 100, False, risk_free_rate, volatility, time), 3)]),
-
-        Portfolio("Strangle", [], [
-            (Option(OptionType.CALL, OrderType.BUY, 80, 100,
-             False, risk_free_rate, volatility, time), 1),
-            (Option(OptionType.PUT, OrderType.BUY, 120, 100, False, risk_free_rate, volatility, time), 1)]),
-
-        Portfolio("Covered Call", [
-            (Stock(100), 1)],
-            [(Option(OptionType.CALL, OrderType.SELL, 80, 100, False, risk_free_rate, volatility, time), 1)])
-    ]
-
-    return portfolios
-
-
 pages = {
     "Home": "home",
     "Create Portfolio": "create_portfolio",
@@ -366,7 +362,7 @@ if selection == "Example Portfolios":
         value=1.0, step=0.1
     )
 
-    portfolios = get_example_portfolios(risk_free_rate, volatility, time)
+    portfolios = Utils.get_example_portfolios(risk_free_rate, volatility, time)
 
     for portfolio in portfolios:
         portfolio.visualize(-100, 200, 5, True)
@@ -374,7 +370,7 @@ if selection == "Example Portfolios":
 elif selection == "Create Portfolio":
     portfolio = Portfolio("Portfolio", [], [])
     st.title("Create a New Portfolio")
-    num_options, num_stocks, stock_price, option_details, risk_free_rate, volatility, time, auto_option_pricing = get_portfolio_details()
+    num_options, num_stocks, stock_price, option_details, risk_free_rate, volatility, time, auto_option_pricing = Utils.get_portfolio_details()
     portfolio.add_stock(Stock(stock_price), num_stocks)
 
     for i in range(num_options):
@@ -389,10 +385,9 @@ elif selection == "Create Portfolio":
 
     portfolio.visualize(-100, 200, 5, True)
 
-
 elif selection == "Black Scholes Model":
     st.title("Black Scholes Model")
-    black_scholes_analysis()
+    Utils.black_scholes_analysis()
 
 elif selection == "Home":
     st.title("Welcome to Option Visualizer")
